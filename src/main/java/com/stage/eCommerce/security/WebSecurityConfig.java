3bef84da5b9207a7602a3989c683e419e7d781dc
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-16 16:06:39 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/stage/eCommerce/security/WebSecurityConfig.java
+++ src/main/java/com/stage/eCommerce/security/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+			.authorizeRequests()
 			.antMatchers("/authentification/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
