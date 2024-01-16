3dfeedf7ffc36505fff37275723e7f7f561e6ce2
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-16 16:11:46 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/stage/eCommerce/security/WebSecurityConfig.java
+++ src/main/java/com/stage/eCommerce/security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 import com.stage.eCommerce.security.jwt.AuthEntryPointJwt;
 import com.stage.eCommerce.security.jwt.AuthTokenFilter;
